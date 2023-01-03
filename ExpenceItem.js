f3f6f666427fc70e613c46ba0d5d8d53d2d4290b
Rachit3819 <117101791+Rachit3819@users.noreply.github.com>
2023-01-03 22:26:06 +0530

    Add files via upload

---
+++ ExpenceItem.js
@@ -0,0 +1,18 @@
+function ExpenceItem(){
+    return (
+    <div>
+        <div>
+            <h2>ExpenceItem</h2>
+            </div>
+        <div>
+            <h2>Food RS 10 </h2>
+            </div>
+            <div>
+                <h2>Petrol Rs 100</h2>
+            </div>
+            <div>
+                <h2>Movies Rs 100</h2>
+            </div>
+        </div>)
+}
+export default ExpenceItem;
